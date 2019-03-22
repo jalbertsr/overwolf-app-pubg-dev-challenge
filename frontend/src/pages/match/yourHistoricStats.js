d212d7969e459f7537104f6456f7347869cbc458
Joan Albert Segura <jalbertsr@protonmail.ch>
2019-03-22 01:40:03 +0100

    calculate avarage stats

--- frontend/src/pages/match/yourHistoricStats.js
+++ frontend/src/pages/match/yourHistoricStats.js
@@ -9,7 +9,10 @@ import {
 } from '../../common/services/apiService';
 
 class YourHistoricStats extends Component {
-  state = {};
+  state = {
+    avarageStats: {},
+    loading: true,
+  };
 
   async componentDidMount() {
     const { accountId, map } = this.props;
@@ -17,7 +20,12 @@ class YourHistoricStats extends Component {
     const arrayOfMatches = relationships.matches.data.map(match => match.id);
     const listOfAllMatches = await this.fetchAllMatches(arrayOfMatches);
     const filteredMatchesByMap = this.filterMatchesByMap(map, listOfAllMatches);
-    console.log(filteredMatchesByMap);
+    const playerMatchesStats = this.extractPlayerMatchesStatsByAccountId(
+      filteredMatchesByMap,
+      accountId,
+    );
+    const avarageStats = this.createAvarageStats(playerMatchesStats);
+    this.setState({ avarageStats, loading: false });
   }
 
   fetchAllMatches = async arrayOfMatches => {
@@ -30,8 +38,82 @@ class YourHistoricStats extends Component {
       match => MAP_NAMES[match.data.attributes.mapName] === mapName,
     );
 
+  extractPlayerMatchesStatsByAccountId = (matches, accountId) => {
+    const playerMatchesData = [];
+    for (let i = 0; i < matches.length; i++) {
+      const matchData = matches[i]['included'];
+      for (let j = 0; j < matchData.length; j++) {
+        const playerData = matchData[j];
+        if (playerData['type'] === 'participant') {
+          const playerStats = playerData['attributes']['stats'];
+          if (playerStats.playerId === accountId) {
+            playerMatchesData.push(playerStats);
+            break;
+          }
+        }
+      }
+    }
+    return playerMatchesData;
+  };
+
+  createAvarageStats = playerMatchesStats => {
+    const numberOfMatches = playerMatchesStats.length;
+    const aggregatedData = {
+      ...Object.keys(playerMatchesStats[0]).reduce((acc, stat) => {
+        acc[stat] = 0;
+        return acc;
+      }, {}),
+      deathType: {
+        byplayer: 0,
+        alive: 0,
+        suicide: 0,
+        logout: 0,
+      },
+    };
+
+    playerMatchesStats.forEach(stats => {
+      Object.keys(stats).forEach(stat => {
+        if (stat === 'deathType') {
+          if (stats.deathType === 'byplayer')
+            aggregatedData.deathType.byplayer++;
+          else if (stats.deathType === 'alive')
+            aggregatedData.deathType.alive++;
+          else if (stats.deathType === 'logout')
+            aggregatedData.deathType.logout++;
+          else if (stats.deathType === 'suicide')
+            aggregatedData.deathType.suicide++;
+        } else {
+          aggregatedData[stat] += stats[stat];
+        }
+      });
+    });
+
+    delete aggregatedData.name;
+    delete aggregatedData.playerId;
+
+    const avarageStats = Object.keys(aggregatedData).reduce((acc, stat) => {
+      stat === 'deathType'
+        ? (acc.deathType = aggregatedData.deathType) // this is not avarage xd
+        : (acc[stat] = aggregatedData[stat] / numberOfMatches);
+      return acc;
+    }, {});
+
+    return avarageStats;
+  };
+
   render() {
-    return <h2>This is your avarage performance in this map</h2>;
+    const { map } = this.props;
+    const { avarageStats, loading } = this.state;
+    return (
+      <React.Fragment>
+        <h2>{`Your avarage performance in ${map}`}</h2>
+        {loading ? (
+          <p>loading...</p>
+        ) : (
+          <div>{JSON.stringify(avarageStats)}</div>
+        )}
+      </React.Fragment>
+    );
   }
 }
 
